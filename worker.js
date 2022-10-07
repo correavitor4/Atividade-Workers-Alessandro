723bba6c86babd8c4579b949daa966ebaaceb1d5
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2022-10-07 20:37:43 -0300

    Resolvido

--- worker.js
+++ worker.js
@@ -1,46 +1,44 @@
 //Define some variables
 const dictionary = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
+let stop = false; // When this variable is true, the function stops to try to find the hash
 
 self.onmessage = (e) => {
-    console.log('Message received from main script');
+    if(e.data == 'stop'){
+        stop = true;
+        return;
+    }
+    console.log('Starting with letter ' + e.data[1]);
     const hash = e.data[0];
     const letter = e.data[1];
     searchHash(hash, letter);
 }
 
 async function digestMessage(message) {
-    const encoder = new TextEncoder();
-    const data = encoder.encode(message);
-    const hash = await crypto.subtle.digest('SHA-256', data);
-    return hash;
+    const utf8 = new TextEncoder().encode(message);
+    return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+        .map((bytes) => bytes.toString(16).padStart(2, '0'))
+        .join('');
+        return hashHex;
+    });
 }
 
-const searchHash = async (hash, letter) => {
+const searchHash = async (hash, letter) => {    
     let finded = false;
-    const finalWord = letter + letter + letter + letter + letter
-    let currentWord = letter;
-    //If is the first time
-    digestMessage(currentWord).then((hashWord) => {
+    const finalWord = letter + dictionary[dictionary.length - 1].repeat(4);
+    let currentWord = letter + dictionary[0].repeat(4);
+
+    while (!finded && !stop) {
+        const hashWord = await digestMessage(currentWord)
         if (hashWord === hash) {
             self.postMessage(currentWord);
             finded = true;
-        }
-    });
-
-    while (!finded ) {
-        currentWord = incrementString(currentWord);
-        digestMessage(currentWord).then((hashWord) => {
-            if (hashWord === hash) {
-                self.postMessage(currentWord);
-                finded = true;
-            }
-        });
+        } 
 
         if (currentWord === finalWord) {
             break;
-        }
-        
+        }    
         currentWord = incrementString(currentWord);        
     }
 }
@@ -49,7 +47,7 @@ const incrementString = (str) => {
     let lastChar = str.charAt(str.length - 1);
     if(lastChar === dictionary[dictionary.length - 1]) {
         if(str.length === 1) {
-            return dictionary[0] + dictionary[0];
+            return str;
         }
         return (incrementString(str.substring(0, str.length - 1)) + dictionary[0]);
     }
